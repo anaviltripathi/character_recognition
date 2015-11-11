c80e7433c9e4c5282789fc1a9263693e7d72315b
Anavil Tripathi <anaviltripathi@gmail.com>
2015-11-10 21:02:59 -0600

    image resizing and crop added

--- reco.py
+++ reco.py
@@ -14,93 +14,88 @@ import numpy
 
 #f = open('./F0000_14/C0000_14.CLS')
 
+def cropImage(im, crop_ratio):
+    imx, imy = im.shape
+    return im[imx/crop_ratio: -imx/crop_ratio, imy/crop_ratio: -imy/crop_ratio]
+
 def loadImage(image):
     #reading image using scipy
     im = misc.imread(image, flatten=1)
+    per = 80
+    im = cropImage(im, 3)
+    im = misc.imresize(im, per)
+    #misc.imshow(im)
+    #print im.shape
     im = im.flatten()
-    #image_size = len(im)
-    #new_image = numpy.zeros(128*128, dtype=float)
-    #print(len(new_image))
-    #print(image_size)
-    #for l in im:
-    #    print("l ",l.flatten()[:])
-    #    new_image
-
-    #new_image.flatten()
-    #print(new_image)
-    #pix = numpy.array(im)
-    #misc.imshow(image)
-
-    #normal reading without using any special libraray
-    #with open(image, 'rb') as f:
-    #    im = bytearray(f.read())
-
-
-    #writing into a new bmp file
-    #f = open('newfile.bmp', 'wb')
-    #f.write(image)
-
-
-    #reading using PIL
-    #im = Image.open(image)
-
-    #print(pix)
-
-    #print(im)
-    #print image #prints something incomprehensible
-
     return im
-    #image.rotate(180).show()
-
-    #writing into a new bmp file
-    #f = open('newfile.bmp', 'wb')
-    #f.write(image)
-
-    #dataset size calculation
 
 def fileLister(path):
-    return glob.glob(path)
+    import glob2
+    files = glob2.glob(path)
+    return sorted(files)
 
 
+#def image_resize(image):
 
 
 
 if __name__ == "__main__":
 
-    dataset_list = fileLister('./F0000_14/*.CLS')
+    dataset_list = fileLister('../character_dataset/sd_nineteen/**/D*.CLS')
     #print dataset_list
+
     dataset_size = 0
+
+    #list_of_digit_files = fileLister('../character_dataset/sd_nineteen/HSF_4/**/*.bmp')
+
+    #loadImage(list_of_digit_files[0])
 
     for i in range(len(dataset_list)):
         with open(dataset_list[i],'r') as f:
             dataset_size += int(f.readline()[:-1])
 
 
-    #print "Data size is :", dataset_size
-    list_of_files = fileLister('./F0000_14/*.bmp')
+    print "Data size is :", dataset_size
+
+    list_of_files = fileLister('../character_dataset/sd_nineteen/HSF_4/**/*_*_*_*_D*_*_*_*_*.bmp')
+
+    #print list_of_files[:1000]
     labels = []
     for file_name in list_of_files:
         labels.append(file_name.split('_')[-2])
 
-    #print(labels)
+
+    #print labels
+    #
+    #
+
     image_name = list_of_files[0]
 
     initializer_image = loadImage(image_name)
     #print(len(initializer_image))
 
-    net = buildNetwork(len(initializer_image), 100, 62)
+    net = buildNetwork(len(initializer_image), 15, 10)
+    print("network built")
 
-    ds = SupervisedDataSet(len(initializer_image) ,62)
+
+    ds = SupervisedDataSet(len(initializer_image) ,10)
     for i in range(dataset_size):
         ds.addSample(loadImage(list_of_files[i]), (ord(labels[i]),))
+    print("data added")
 
     trainer = BackpropTrainer(net, ds)
+    print("trainer initialized")
 
     error = 10
     iteration = 0
-    while error > 0.001:
+
+    #exit(0)
+    while iteration < 10:
         error = trainer.train()
         iteration += 1
         print "Iteration: {0} Error {1}".format(iteration, error)
 
     print "\nResult: ", net.activate(loadImage(list_of_files[0]))
+
+
+
